f9ba80a2dbab5cbb3a1f63c4997aaf4f1dbf7775
Giancarlo Danves <gdanves@gmail.com>
2020-10-01 23:27:06 -0300

    I don't know exactly what I'm doing

--- scripts/main.js
+++ scripts/main.js
@@ -15,19 +15,51 @@ friends.forEach(function(friend) {
 b = data.friends[a];
 var card = document.createElement('article');
 card.className = 'card';
+friendscontainer.appendChild(card);
 
 var usericons = document.createElement('footer');
 usericons.className = ('user-icons');
+card.appendChild(usericons);
 
 var figure = document.createElement ('figure');
-figure.innerHTML += `<img class = picturevisitor src = ${b.imgUrl} ></img>`;
-
-
-friendscontainer.appendChild(card);
-card.appendChild(usericons);
 usericons.appendChild(figure);
+
+var photo = document.createElement ('div');
+photo.className = ('picturevisitor');
+photo.style.backgroundImage = `url('${b.imgUrl}')`;
+figure.appendChild(photo);
+
+
+var ratingvisitor = document.createElement ('div');
+ratingvisitor.className = ('ratingvisitor');
+usericons.appendChild(ratingvisitor); //appending 
+
+var stars = b.rate;
+var i = 1;
+while (i  <= 5) {
+
+    if (i <= stars) {
+        ratingvisitor.innerHTML += '<i class="material-icons">star</i>';
+    } else {
+        ratingvisitor.innerHTML += '<i class="material-icons">star_outline</i>';
+    }
+    i++;
+}
+
+var usertext = document.createElement('div');
+usertext.className = ('user-text');
+card.appendChild(usertext);
+
+var friendname = document.createElement('header');
+friendname.innerHTML += `<h3 class = "friendname">${b.name}</h3>`
+usertext.appendChild(friendname);
+
+var bio = document.createElement('p');
+bio.className = ('bio');
+bio.innerHTML = b.description;
+usertext.appendChild(bio);
+
 a++;
-console.log(b);
 }
 ) 
 
