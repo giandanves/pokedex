67413eb5cbf36ba51e51271ec323daf236fb5ca3
Giancarlo Danves <gdanves@gmail.com>
2020-10-08 22:35:05 -0300

    create func to fill Profile and make it reusable

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,14 @@
 import data from './data.js';
 
 //functions 
-
-//numberToStar is a function to convert the rating data, from a number to stars. that's it. ok? bye.
 function numberToStar(rate, stars) {
   for (let i = 0; i < 5; i++) {
-    var starposition;
-
+    const starposition = document.createElement('i');
     if (stars >= i + 1) {
-      starposition = document.createElement('i');
       starposition.textContent += 'star';
     } else if (stars <= i) {
-      starposition = document.createElement('i');
       starposition.textContent += 'star_outline';
     } else {
-      starposition = document.createElement('i');
       starposition.textContent += 'star_half';
     }
     starposition.className = ('material-icons');
@@ -24,32 +18,35 @@ function numberToStar(rate, stars) {
 
 }
 
-//userbox 
-//receiving data of profile's user
+function fillProfileInfo(user) {
+  const profileimage = document.querySelector('#profile-image');
+  profileimage.src = user.imgUrl;
+
+  const welcome = document.querySelector('#welcome-message');
+  welcome.textContent = `Welcome, ${user.name}!`;
+
+  const visits = document.querySelector('#profile-visits');
+  visits.textContent = `${user.profileVisits} profile visits last week`;
+
+  const ratedBy = document.querySelector('#profile-rated-by');
+  ratedBy.textContent = `${user.ratedBy} people rated your profile`;
+
+  const ratinguser = document.querySelector("#ratinguser");
+  var stars = user.rate;
+  numberToStar(ratinguser, stars);
+
+}
+
+//userbox
 const user = data.profile;
-//adding profile photo
-const profileimage = document.querySelector('#profile-image');
-profileimage.src = user.imgUrl;
-//adding welcome message
-const welcome = document.querySelector('#welcome-message');
-welcome.textContent = `Welcome, ${user.name}!`;
-//adding total visits and how many users rated the user profile
-const visits = document.querySelector('#profile-visits');
-visits.textContent = `${user.profileVisits} profile visits last week`;
-const ratedBy = document.querySelector('#profile-rated-by');
-ratedBy.textContent = `${user.ratedBy} people rated your profile`;
+fillProfileInfo(user);
 
-//adding the user's rate 
-const ratinguser = document.querySelector("#ratinguser");
-var stars = user.rate;
-numberToStar(ratinguser, stars);
 
-//bringing the friends container to manipulate it and create all the friends cards. 
 const friendscontainer = document.querySelector('#friends-container');
 const friends = data.friends;
 
-//here, a bigger function that creates a card for every friend in data.js, with their names, pictures, descriptions and rating.  
-friends.forEach(function (friend) {
+//Friends Cards
+friends.forEach(function fillFriendsCards(friend) {
   var card = document.createElement('article');
   card.className = 'card';
   friendscontainer.appendChild(card);
@@ -61,7 +58,7 @@ friends.forEach(function (friend) {
   var figure = document.createElement('figure');
   usericons.appendChild(figure);
 
-  var photo = document.createElement('div');
+  var photo = document.createElement('img');
   photo.className = ('picturevisitor');
   photo.style.backgroundImage = `url('${friend.imgUrl}')`;
   figure.appendChild(photo);
@@ -69,9 +66,9 @@ friends.forEach(function (friend) {
 
   var ratingvisitor = document.createElement('div');
   ratingvisitor.className = ('ratingvisitor');
-  stars = user.rate;
+  var stars = user.rate;
   numberToStar(ratingvisitor, stars);
-  usericons.appendChild(ratingvisitor); //appending 
+  usericons.appendChild(ratingvisitor);
 
   var usertext = document.createElement('div');
   usertext.className = ('user-text');
