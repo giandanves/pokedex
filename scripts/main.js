082942c40d44ee8bb4dac6c92429ece73eea36f2
Giancarlo Danves <gdanves@gmail.com>
2020-10-11 00:08:44 -0300

    improving code reusability and another fixes

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,8 @@
 import data from './data.js';
 
 //functions 
-function numberToStar(rate, stars) {
+function renderRating(stars) {
+  const rating = document.createElement('div');
   for (let i = 0; i < 5; i++) {
     const starposition = document.createElement('i');
     if (stars >= i + 1) {
@@ -12,92 +13,77 @@ function numberToStar(rate, stars) {
       starposition.textContent += 'star_half';
     }
     starposition.className = ('material-icons');
-    rate.appendChild(starposition);
-
+    rating.appendChild(starposition);
   }
-
+  return rating;
 }
 
-function fillProfileInfo(user) {
+function renderProfileInfo(user) {
   const profileimage = document.querySelector('#profile-image');
   profileimage.src = user.imgUrl;
 
-  const welcome = document.querySelector('#welcome-message');
-  welcome.textContent = `Welcome, ${user.name}!`;
+  const welcomeMessage = document.querySelector('#welcome-message');
+  welcomeMessage.textContent = `Welcome, ${user.name}!`;
 
-  const visits = document.querySelector('#profile-visits');
-  visits.textContent = `${user.profileVisits} profile visits last week`;
+  const profileVisits = document.querySelector('#profile-visits');
+  profileVisits.textContent = `${user.profileVisits} profile visits last week`;
 
-  const ratedBy = document.querySelector('#profile-rated-by');
-  ratedBy.textContent = `${user.ratedBy} people rated your profile`;
+  const profileRatedBy = document.querySelector('#profile-rated-by');
+  profileRatedBy.textContent = `${user.ratedBy} people rated your profile`;
 
-  const ratinguser = document.querySelector("#ratinguser");
-  var stars = user.rate;
-  numberToStar(ratinguser, stars);
+  const userRating = document.querySelector("#user-rating");
+  const rating = renderRating(user.rate);
+  userRating.appendChild(rating);
 
 }
 
-//userbox
-const user = data.profile;
-fillProfileInfo(user);
-
-
-const friendscontainer = document.querySelector('#friends-container');
-const friends = data.friends;
-
-//Friends Cards
-friends.forEach(function fillFriendsCards(friend) {
-  var card = document.createElement('article');
+function renderFriend(friend) {
+  const card = document.createElement('article');
   card.className = 'card';
-  friendscontainer.appendChild(card);
 
-  var usericons = document.createElement('footer');
-  usericons.className = ('user-icons');
-  card.appendChild(usericons);
+  const friendicons = document.createElement('footer');
+  friendicons.className = 'friend-icons';
+  card.appendChild(friendicons);
 
-  var figure = document.createElement('figure');
-  usericons.appendChild(figure);
+  const figure = document.createElement('figure');
+  friendicons.appendChild(figure);
 
-  var photo = document.createElement('img');
-  photo.className = ('picturevisitor');
+  const photo = document.createElement('img');
+  photo.className = 'picturevisitor';
   photo.style.backgroundImage = `url('${friend.imgUrl}')`;
   figure.appendChild(photo);
 
 
-  var ratingvisitor = document.createElement('div');
-  ratingvisitor.className = ('ratingvisitor');
-  var stars = user.rate;
-  numberToStar(ratingvisitor, stars);
-  usericons.appendChild(ratingvisitor);
+  const ratingVisitor = document.createElement('div');
+  ratingVisitor.className = 'ratingvisitor';
+  const rating = renderRating(friend.rate);
+  ratingVisitor.appendChild(rating);
+  friendicons.appendChild(ratingVisitor);
 
-  var usertext = document.createElement('div');
-  usertext.className = ('user-text');
+  const usertext = document.createElement('div');
+  usertext.className = 'user-text';
   card.appendChild(usertext);
 
-  var friendname = document.createElement('header');
+  const friendname = document.createElement('header');
   friendname.innerHTML += `<h3 class = "friendname">${friend.name}</h3>`
   usertext.appendChild(friendname);
 
-  var bio = document.createElement('p');
-  bio.className = ('bio');
+  const bio = document.createElement('p');
+  bio.className = 'bio';
   bio.innerHTML = friend.description;
   usertext.appendChild(bio);
 
+  return card;
 }
-);
 
+function renderFriendsList(friends) {
+  const friendscontainer = document.querySelector('#friends-container');
+  friends.forEach(function (friend) {
+    const cardFriend = renderFriend(friend);
+    friendscontainer.appendChild(cardFriend);
+  })
 
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+renderProfileInfo(data.profile);
+renderFriendsList(data.friends);
