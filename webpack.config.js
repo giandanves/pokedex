d662bb775547d6af7fabccb995c110733f3c740d
Giancarlo Danves <gdanves@gmail.com>
2021-04-16 11:41:07 -0300

    prettier changes

--- webpack.config.js
+++ webpack.config.js
@@ -2,33 +2,35 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-
 module.exports = {
-    mode: "development",
-    entry: './src/index.js',
+  mode: 'development',
+  entry: './src/index.js',
 
-    plugins: [new HtmlWebpackPlugin({ title: 'Izipizi', }), new MiniCssExtractPlugin({ filename: 'style.css' }),
-    ],
+  plugins: [
+    new HtmlWebpackPlugin({ title: 'Izipizi' }),
+    new MiniCssExtractPlugin({ filename: 'style.css' }),
+  ],
 
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist',
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.scss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader, "css-loader", "sass-loader",
-                ]
-            },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
         ],
-    },
-
+      },
+    ],
+  },
 };
