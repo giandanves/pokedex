bfbcd1f4982b6814a675ecac46a5d41f024d3ef1
Giancarlo Danves <gdanves@gmail.com>
2021-04-20 19:54:14 -0300

    making tests if method's working

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './style.scss';
 import Storage from './storage.js';
+import localStorage from './localstorage.js';
 
 class TestObject {
   constructor(id, date, content) {
@@ -8,3 +9,16 @@ class TestObject {
     this.content = content;
   }
 }
+
+const one = new TestObject('15', '25', 'bola');
+const two = new TestObject('16', '25', 'rato');
+const three = new TestObject('17', 'bola', 'arroz');
+const four = new TestObject('22', 'bola', 'arroz');
+const five = new TestObject('22', 'ball', 'rice');
+
+const storage = new Storage();
+storage.addMany([one, two, three]);
+
+console.log(storage);
+console.log(storage.addMany([four, five]));
+console.log(storage);
