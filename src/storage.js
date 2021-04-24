c9de64e2ecb5f2c6812b24774e32f74e44c88214
Giancarlo Danves <gdanves@gmail.com>
2021-04-24 01:56:23 -0300

    making storage system async with the same changes of localstorage system, created a getHistory

--- src/storage.js
+++ src/storage.js
@@ -3,16 +3,22 @@ class Storage {
     this.history = [];
   }
 
+  get getHistory() {
+    return new Promise((resolve) => resolve(this.history));
+  }
   save(operation) {
-    const idExists = this.history.find(
-      (saved) => operation.id === saved.id,
-    );
+    return this.getHistory.then((history) => {
+      const idExists = history.find(
+        (saved) => operation.id === saved.id,
+      );
 
-    if (idExists) {
-      throw new Error('This ID already exists on memory');
-    } else {
-      this.history.push(operation);
-    }
+      if (idExists) {
+        throw new Error('This ID already exists on memory');
+      } else {
+        this.history.push(operation);
+        return operation;
+      }
+    });
   }
 
   find({ id, date, content }) {
@@ -23,83 +29,93 @@ class Storage {
     ) {
       return null;
     } else {
-      return this.history.find(
+      return this.getHistory.then((history) => {
+        return history.find(
+          (object) =>
+            (object.id == id || id == undefined) &&
+            (object.date == date || date == undefined) &&
+            (object.content == content || content == undefined),
+        );
+      });
+    }
+  }
+
+  first() {
+    return this.getHistory.then((history) => {
+      return history[history.length - 1];
+    });
+  }
+
+  findAll({ id, date, content }) {
+    return this.getHistory.then((history) => {
+      return history.filter(
         (object) =>
           (object.id == id || id == undefined) &&
           (object.date == date || date == undefined) &&
           (object.content == content || content == undefined),
       );
-    }
-  }
-
-  first() {
-    return this.history[this.history.length - 1];
-  }
-
-  findAll({ id, date, content }) {
-    return this.history.filter(
-      (object) =>
-        (object.id == id || id == undefined) &&
-        (object.date == date || date == undefined) &&
-        (object.content == content || content == undefined),
-    );
+    });
   }
 
   delete(id) {
-    const filteredHistory = this.history.filter(
-      (object) => object.id !== id,
-    );
-    const deletedItem = this.history.find(
-      (object) => object.id === id,
-    );
-    if (!deletedItem) {
-      throw new Error('id is not registered');
-    }
-    this.history = filteredHistory;
-    return deletedItem;
+    return this.getHistory.then((history) => {
+      const filteredHistory = history.filter(
+        (object) => object.id !== id,
+      );
+      const deletedItem = history.find((object) => object.id === id);
+      if (!deletedItem) {
+        throw new Error('id is not registered');
+      }
+      this.history = filteredHistory;
+      return deletedItem;
+    });
   }
   deleteMany(ids) {
-    const filteredHistory = this.history.filter(
-      (object) => !ids.includes(object.id),
-    );
-    const deletedItems = this.history.filter((object) =>
-      ids.includes(object.id),
-    );
-    if (deletedItems.length !== ids.length) {
-      throw new Error('Some id is not registered');
-    }
-    this.history = filteredHistory;
-    return deletedItems;
+    return this.getHistory.then((history) => {
+      const filteredHistory = history.filter(
+        (object) => !ids.includes(object.id),
+      );
+      const deletedItems = history.filter((object) =>
+        ids.includes(object.id),
+      );
+      if (deletedItems.length !== ids.length) {
+        throw new Error('Some id is not registered');
+      }
+      this.history = filteredHistory;
+      return deletedItems;
+    });
   }
 
   addMany(array) {
     const addable = [];
-    const history = this.history;
 
     for (let i of array) {
       if (array.filter((item) => item.id === i.id).length > 1) {
         throw new Error('Repeated IDs provided');
       }
     }
+    return this.getHistory.then((history) => {
+      array.find((object) => {
+        let sameIDCheck = history.find(
+          (element) => element.id == object.id,
+        );
 
-    array.find((object) => {
-      let sameIDCheck = history.find(
-        (element) => element.id == object.id,
-      );
+        if (sameIDCheck) {
+          throw new Error('Repeated IDs');
+        } else {
+          addable.push(object);
+        }
+      });
 
-      if (sameIDCheck) {
-        throw new Error('Repeated IDs');
+      if (addable.length != array.length) {
+        throw new Error('Cannot add all the provided items');
       } else {
-        addable.push(object);
+        addable.forEach((object) => history.push(object));
       }
-    });
 
-    if (addable.length != array.length) {
-      throw new Error('Cannot add all the provided items');
-    } else {
-      addable.forEach((object) => this.history.push(object));
-    }
-    return addable;
+      this.history = history;
+      return addable;
+    });
   }
 }
 
