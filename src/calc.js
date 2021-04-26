6fed141f12e0385b27a5c8733c846ed9edcaf9e8
Giancarlo Danves <gdanves@gmail.com>
2021-04-26 12:58:09 -0300

    changes in calc function

--- src/calc.js
+++ src/calc.js
@@ -1,6 +1,7 @@
 function calc(calcString) {
   const operators = [];
   const numbers = [];
+
   let numberFinder = '';
   for (let char of calcString) {
     if (isNaN(char)) {
@@ -14,29 +15,19 @@ function calc(calcString) {
   numbers.push(numberFinder);
   let result = numbers[0];
 
+  const operations = {
+    '+': (n1, n2) => n1 * 1 + n2 * 1,
+    '-': (n1, n2) => n1 * 1 - n2 * 1,
+    '*': (n1, n2) => n1 * n2,
+    '/': (n1, n2) => n1 / n2,
+  };
+
   for (let i = 0; i < operators.length; i++) {
     if (i < operators.length) {
-      result = operationMaker(result, operators[i], numbers[i + 1]);
+      result = operations[operators[i]](result, numbers[i + 1]);
     }
   }
-  console.log(result);
-}
-
-function operationMaker(n1, op, n2) {
-  if (op == '+') {
-    return n1 * 1 + n2 * 1;
-  }
-  if (op == '-') {
-    return n1 * 1 - n2 * 1;
-  }
-
-  if (op == '*') {
-    return n1 * n2;
-  }
-
-  if (op == '/') {
-    return n1 / n2;
-  }
+  return result;
 }
 
 export default calc;
