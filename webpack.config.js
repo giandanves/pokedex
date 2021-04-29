06be150bc80bc29dca489e782e27a52c0f0a25cb
Giancarlo Danves <gdanves@gmail.com>
2021-04-28 22:37:11 -0300

    trying to load svg

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,14 @@ module.exports = {
           'sass-loader',
         ],
       },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svg-url-loader',
+          },
+        ],
+      },
 
       {
         test: /\.m?js$/,
