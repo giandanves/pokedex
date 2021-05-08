de85171764cec92c53749e04d056cba3b73d4e05
Giancarlo Danves <gdanves@gmail.com>
2021-05-07 21:56:43 -0300

    using for in to add event listeners to buttons

--- src/scripts/views/calculator.js
+++ src/scripts/views/calculator.js
@@ -1,37 +1,27 @@
+import buttons from './buttons';
+
 class View {
   constructor(onClick) {
-    this.zero = document.querySelector('#n0');
-    this.one = document.querySelector('#n1');
-    this.two = document.querySelector('#n2');
-    this.three = document.querySelector('#n3');
-    this.four = document.querySelector('#n4');
-    this.five = document.querySelector('#n5');
-    this.six = document.querySelector('#n6');
-    this.seven = document.querySelector('#n7');
-    this.eight = document.querySelector('#n8');
-    this.nine = document.querySelector('#n9');
-
-    this.zero.addEventListener('click', this.onButtonClick);
-    this.one.addEventListener('click', this.onButtonClick);
-    this.two.addEventListener('click', this.onButtonClick);
-    this.three.addEventListener('click', this.onButtonClick);
-    this.four.addEventListener('click', this.onButtonClick);
-    this.five.addEventListener('click', this.onButtonClick);
-    this.six.addEventListener('click', this.onButtonClick);
-    this.seven.addEventListener('click', this.onButtonClick);
-    this.eight.addEventListener('click', this.onButtonClick);
-    this.nine.addEventListener('click', this.onButtonClick);
+    this.buttons = [];
+    for (var prop in buttons) {
+      const buttonID = document.querySelector(`#${prop}`);
+      buttonID.addEventListener('click', this.onButtonClick);
+      this.buttons.push(buttonID);
+    }
 
     this.onClick = onClick;
   }
 
   onButtonClick = (e) => {
-    const selection = e.currentTarget.id.replace(/\D/g, '');
+    const selection = buttons[e.currentTarget.id];
     console.log(selection);
     this.onClick(selection);
   };
 
-  renderNumbers(state) {
+  renderCalculum(state, stateLog) {
+    this.calculumLog = document.querySelector('#calculum-log');
+    this.calculumLog.textContent = stateLog;
+
     this.calculumRendered = document.querySelector(
       '#calculum-rendered',
     );
