2ac4f969dca40d15f6a86dfbf899c55e196fecce
Giancarlo Danves <gdanves@gmail.com>
2021-05-11 12:44:23 -0300

    setting interactivity between controller and storage

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import './style/style.scss';
 import View from './scripts/views/calculator.js';
 import Controller from './scripts/controllers/calculator.js';
+import Storage from './scripts/storage/storage.js';
 
 let controller;
 let view;
+let storage = new Storage();
 
 function onClick(btn) {
   view.renderCalculum(
     controller.updateState(btn),
     controller._stateLog,
   );
+
+  if (controller.storedOperation) {
+    storage.save(controller.storedOperation);
+    console.dir(storage);
+  }
 }
 
 function initApp() {
