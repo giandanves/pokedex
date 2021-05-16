94a89f9fe75a4e51023ea0e4b49230b03ac8ed1e
Giancarlo Danves <gdanves@gmail.com>
2021-05-16 18:02:58 -0300

    stats Index calling renderStatsPage

--- src/stats.js
+++ src/stats.js
@@ -1,6 +1,6 @@
 import './style/style.scss';
 import LocalStorage from './scripts/storage/localstorage.js';
-import updateStats from './scripts/controllers/stats.js';
+import renderStatsPage from './scripts/controllers/stats.js';
 
 let storage = new LocalStorage();
-updateStats(storage);
+renderStatsPage(storage);
