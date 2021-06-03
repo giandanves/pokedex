2ca37e5214ae7ae5d5857f2be3fcdf69c825d4f4
Giancarlo Danves <gdanves@gmail.com>
2021-06-02 23:29:45 -0300

    check-box components

---
+++ my-app/src/weight-checkbox.js
@@ -0,0 +1,22 @@
+export function WeightCheckBox() {
+return (
+    <>
+  <section>
+    <p>Weight</p>
+    <input type="checkbox" id="weight=1" name="weight" />
+    <label>1</label>
+    <input type="checkbox" id="weight=2" name="weight" />
+    <label>2</label>
+    <input type="checkbox" id="weight=3" name="weight" />
+    <label>3</label>
+    <input type="checkbox" id="weight=4" name="weight" />
+    <label>4</label>
+    <input type="checkbox" id="weight=5" name="weight" />
+    <label>5</label>
+  </section>
+  </>
+);
+
+}
+
+
