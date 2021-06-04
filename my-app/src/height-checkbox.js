229742daa012a1cc0d1b8267381764e82d908534
Giancarlo Danves <gdanves@gmail.com>
2021-06-04 12:54:05 -0300

    using ref and state to control checkbox with react

--- my-app/src/height-checkbox.js
+++ my-app/src/height-checkbox.js
@@ -1,17 +1,67 @@
+import { useState, useRef } from "react";
+
 export function HeightCheckBox() {
+  const [heightBoxesCheckeds, setHeightBoxesCheckeds] = useState([]);
+  const refHeight1 = useRef(null);
+  const refHeight2 = useRef(null);
+  const refHeight3 = useRef(null);
+  const refHeight4 = useRef(null);
+  const refHeight5 = useRef(null);
+
+  const handleCheckBox = (ref) => {
+    if (ref.current.checked) {
+      setHeightBoxesCheckeds((prev) => [...prev, ref.current]);
+    } else {
+      const filteredCheckBox = heightBoxesCheckeds.filter(
+        (checkbox) => checkbox !== ref.current
+      );
+      setHeightBoxesCheckeds(filteredCheckBox);
+    }
+  };
+
   return (
     <>
       <section>
         <p>Height</p>
-        <input type="checkbox" id="height=1" name="height" />
+        <input
+          type="checkbox"
+          id="height=1"
+          name="height"
+          ref={refHeight1}
+          onChange={() => handleCheckBox(refHeight1)}
+        />
         <label>1</label>
-        <input type="checkbox" id="height=2" name="height" />
+        <input
+          type="checkbox"
+          id="height=2"
+          name="height"
+          ref={refHeight2}
+          onChange={() => handleCheckBox(refHeight2)}
+        />
         <label>2</label>
-        <input type="checkbox" id="height=3" name="height" />
+        <input
+          type="checkbox"
+          id="height=3"
+          name="height"
+          ref={refHeight3}
+          onChange={() => handleCheckBox(refHeight3)}
+        />
         <label>3</label>
-        <input type="checkbox" id="height=4" name="height" />
+        <input
+          type="checkbox"
+          id="height=4"
+          name="height"
+          ref={refHeight4}
+          onChange={() => handleCheckBox(refHeight4)}
+        />
         <label>4</label>
-        <input type="checkbox" id="height=5" name="height" />
+        <input
+          type="checkbox"
+          id="height=5"
+          name="height"
+          ref={refHeight5}
+          onChange={() => handleCheckBox(refHeight5)}
+        />
         <label>5</label>
       </section>
     </>
--- my-app/src/weight-checkbox.js
+++ my-app/src/weight-checkbox.js
@@ -1,17 +1,66 @@
+import { useState, useRef } from "react";
+
 export function WeightCheckBox() {
+  const [weightBoxesCheckeds, setWeightBoxesCheckeds] = useState([]);
+  const refWeight1 = useRef(null);
+  const refWeight2 = useRef(null);
+  const refWeight3 = useRef(null);
+  const refWeight4 = useRef(null);
+  const refWeight5 = useRef(null);
+
+  const handleCheckBox = (ref) => {
+    if (ref.current.checked) {
+      setWeightBoxesCheckeds((prev) => [...prev, ref.current]);
+    } else {
+      const filteredCheckBox = weightBoxesCheckeds.filter(
+        (checkbox) => checkbox !== ref.current
+      );
+      setWeightBoxesCheckeds(filteredCheckBox);
+    }
+  };
   return (
     <>
       <section>
         <p>Weight</p>
-        <input type="checkbox" id="weight=1" name="weight" />
+        <input
+          type="checkbox"
+          id="weight=1"
+          name="weight"
+          ref={refWeight1}
+          onChange={() => handleCheckBox(refWeight1)}
+        />
         <label>1</label>
-        <input type="checkbox" id="weight=2" name="weight" />
+        <input
+          type="checkbox"
+          id="weight=2"
+          name="weight"
+          ref={refWeight2}
+          onChange={() => handleCheckBox(refWeight2)}
+        />
         <label>2</label>
-        <input type="checkbox" id="weight=3" name="weight" />
+        <input
+          type="checkbox"
+          id="weight=3"
+          name="weight"
+          ref={refWeight3}
+          onChange={() => handleCheckBox(refWeight3)}
+        />
         <label>3</label>
-        <input type="checkbox" id="weight=4" name="weight" />
+        <input
+          type="checkbox"
+          id="weight=4"
+          name="weight"
+          ref={refWeight4}
+          onChange={() => handleCheckBox(refWeight4)}
+        />
         <label>4</label>
-        <input type="checkbox" id="weight=5" name="weight" />
+        <input
+          type="checkbox"
+          id="weight=5"
+          name="weight"
+          ref={refWeight5}
+          onChange={() => handleCheckBox(refWeight5)}
+        />
         <label>5</label>
       </section>
     </>
