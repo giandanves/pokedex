18d59ebd5c94e41ed581d459c90a0ec5f0655763
Giancarlo Danves <gdanves@gmail.com>
2021-06-04 19:50:36 -0300

    added state, ref and tanta coisa

--- my-app/src/PokemonTypes.js
+++ my-app/src/PokemonTypes.js
@@ -1,9 +1,15 @@
+import {useRef} from 'react'
+import {handleCheckBox} from './handleCheckBox'
+
 export function PokemonTypes(props) {
-  const { types } = props;
+  const { types, poketypesCheckeds, setPoketypesCheckeds } = props;
+  const poketypeRefs = useRef([]);
+
+
   return (
     <section>
       <p>Types</p>
-      {types.map((pokeType) => {
+      {types.map((pokeType, i) => {
         return (
           <>
             <input
@@ -11,6 +17,8 @@ export function PokemonTypes(props) {
               id={`type=${pokeType.name}`}
               name="type"
               key={pokeType.url}
+              ref={(element) => poketypeRefs.current.push(element)}
+              onChange={() => handleCheckBox(poketypeRefs.current[i], poketypesCheckeds, setPoketypesCheckeds)}
             />
             <label key={pokeType.name}>{pokeType.name}</label>
           </>
