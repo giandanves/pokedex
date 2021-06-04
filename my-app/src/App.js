001884e4019af6c07481e02ee821f9232377002e
Giancarlo Danves <gdanves@gmail.com>
2021-06-04 19:51:02 -0300

    making all the search criterias controllable with react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import { useFetch } from "./useFetch";
-import { useState, useRef } from "react";
+import { useState} from "react";
 import { Pokemons } from "./Pokemons";
 import { PokemonTypes } from "./PokemonTypes.js";
 import { handleLoadAndError } from "./HandleLoadAndError";
 import { HeightCheckBox } from "./height-checkbox";
 import { WeightCheckBox } from "./weight-checkbox";
+import {TextBox} from './textBox';
+import {getBoxesChecked} from './handleCheckBox';
 
 const defaultUrl = process.env.REACT_APP_DEFAULT_URL;
 const poketypesUrl = process.env.REACT_APP_POKETYPES_URL;
@@ -13,48 +15,51 @@ function App() {
   let [url, setUrl] = useState(defaultUrl);
   const [loading, pokemons, error] = useFetch(url);
   const types = useFetch(poketypesUrl);
-
-  const form = useRef(null);
-  const inputText = useRef(null);
-
+  const [textBox, setTextBox] = useState("");
+  const [heightBoxesCheckeds, setHeightBoxesCheckeds] = useState([]);
+  const [weightBoxesCheckeds, setWeightBoxesCheckeds] = useState([]);
+  const [poketypesCheckeds, setPoketypesCheckeds] = useState([]);
   const [typeIsLoading, typeResult, typeHasError] = types;
 
   const handleFilter = (filter) => {
-    const formElements = Array.from(form.current);
-    formElements.forEach((element) => {
-      if (element.checked) {
-        filter += `${element.id}&`;
-      }
-    });
+    if (textBox) {
+      filter += `search=${textBox}&`;
+    } 
+    filter+= getBoxesChecked(heightBoxesCheckeds, weightBoxesCheckeds, poketypesCheckeds);
     return filter;
   };
 
   const handleSearch = (e) => {
     e.preventDefault();
-    let filter = "?";
-    if (inputText.current.value) {
-      filter += `search=${inputText.current.value}&`;
-    } else {
-      filter = handleFilter(filter);
-    }
+    let filter = "?"
+    filter = handleFilter(filter);
     url = defaultUrl;
-    console.log(url + filter);
     setUrl(url + filter);
   };
 
   return (
     <>
-      <form id="search-form" ref={form}>
+      <form id="search-form">
         <button type="submit" onClick={(e) => handleSearch(e)}>
           Search
         </button>
-        <input type="text" id="text-area" ref={inputText}></input>
-        <HeightCheckBox />
-        <WeightCheckBox />
+        <TextBox setTextBox={setTextBox}/>
+        <HeightCheckBox
+          heightBoxesCheckeds={heightBoxesCheckeds}
+          setHeightBoxesCheckeds={setHeightBoxesCheckeds}
+        />
+        <WeightCheckBox
+          weightBoxesCheckeds={weightBoxesCheckeds}
+          setWeightBoxesCheckeds={setWeightBoxesCheckeds}
+        />
 
         <section className="typePokemonContainer">
           {handleLoadAndError(typeIsLoading, typeHasError) || (
-            <PokemonTypes types={typeResult.results} />
+            <PokemonTypes
+              types={typeResult.results}
+              poketypesCheckeds={poketypesCheckeds}
+              setPoketypesCheckeds={setPoketypesCheckeds}
+            />
           )}
         </section>
 
