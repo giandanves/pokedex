43abe72a10ea643282750402831ed20ecc1be467
Giancarlo Danves <gdanves@gmail.com>
2021-06-05 18:11:20 -0300

    usinn Array.fill to.. fill it

--- my-app/src/initialCheckboxValues.js
+++ my-app/src/initialCheckboxValues.js
@@ -1,9 +1,3 @@
 export function initialCheckboxValue(i) {
-  const initialArray = new Array(i);
-
-  for (let j = 0; j < i; j++) {
-    initialArray[j] = false;
-  }
-
-  return initialArray;
+  return new Array(i).fill(false);
 }
