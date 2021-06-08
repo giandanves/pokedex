6b2691bb2358149f0c00ed32c6e2388d660ecd16
Giancarlo Danves <gdanves@gmail.com>
2021-06-07 23:23:38 -0300

    returning message in case of empty array

--- my-app/src/Pokemons.js
+++ my-app/src/Pokemons.js
@@ -2,30 +2,36 @@ export function Pokemons(props) {
   const { pokemons } = props;
   return (
     <section>
-      {pokemons.map((pokemon, i) => {
-        const { picture, name, id, type } = pokemon;
-        if (i < 50) {
-          return (
-            <article className="pokemon-card" key={id}>
-              <li>
-                <img className="pokemon-avatar" src={picture} alt={name} />
-                <h2>{name}</h2>
-                <div className="types-grid">
-                  {type.map((poketype, i) => {
-                    if (i === 1) {
-                      return <p>{`- ${poketype}`}</p>;
-                    }
-                    return <p>{poketype}</p>;
-                  })}
-                  <p>{`id: #${id}`}</p>
-                </div>
-              </li>
-            </article>
-          );
-        } else {
-          return <></>;
-        }
-      })}
+      {pokemons ? (
+        <p>
+          No Pokemons matches this criterias, change the filters and try again
+        </p>
+      ) : (
+        pokemons.map((pokemon, i) => {
+          const { picture, name, id, type } = pokemon;
+          if (i < 50) {
+            return (
+              <article className="pokemon-card" key={id}>
+                <li>
+                  <img className="pokemon-avatar" src={picture} alt={name} />
+                  <h2>{name}</h2>
+                  <div className="types-grid">
+                    {type.map((poketype, i) => {
+                      if (i === 1) {
+                        return <p>{`- ${poketype}`}</p>;
+                      }
+                      return <p>{poketype}</p>;
+                    })}
+                    <p>{`id: #${id}`}</p>
+                  </div>
+                </li>
+              </article>
+            );
+          } else {
+            return <></>;
+          }
+        })
+      )}
     </section>
   );
 }
