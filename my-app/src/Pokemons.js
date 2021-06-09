9cefd07e783c973a0bb8cb298e1a4def83c74bb9
Giancarlo Danves <gdanves@gmail.com>
2021-06-09 10:11:48 -0300

    removing limit, now it's handled with limit on url

--- my-app/src/Pokemons.js
+++ my-app/src/Pokemons.js
@@ -9,27 +9,24 @@ export function Pokemons(props) {
       ) : (
         pokemons.map((pokemon, i) => {
           const { picture, name, id, type } = pokemon;
-          if (i < 50) {
-            return (
-              <article className="pokemon-card" key={id}>
-                <li>
-                  <img className="pokemon-avatar" src={picture} alt={name} />
-                  <h2>{name}</h2>
-                  <div className="types-grid">
-                    {type.map((poketype, i) => {
-                      if (i === 1) {
-                        return <p>{`- ${poketype}`}</p>;
-                      }
-                      return <p>{poketype}</p>;
-                    })}
-                    <p>{`id: #${id}`}</p>
-                  </div>
-                </li>
-              </article>
-            );
-          } else {
-            return <></>;
-          }
+
+          return (
+            <article className="pokemon-card" key={id}>
+              <li>
+                <img className="pokemon-avatar" src={picture} alt={name} />
+                <h2>{name}</h2>
+                <div className="types-grid">
+                  {type.map((poketype, i) => {
+                    if (i === 1) {
+                      return <p>{`- ${poketype}`}</p>;
+                    }
+                    return <p>{poketype}</p>;
+                  })}
+                  <p>{`id: #${id}`}</p>
+                </div>
+              </li>
+            </article>
+          );
         })
       )}
     </section>
