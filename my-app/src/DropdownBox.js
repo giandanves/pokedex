a45d059804616255ff97f001d06ee9056c1121ae
Giancarlo Danves <gdanves@gmail.com>
2021-06-09 15:23:15 -0300

    created handleOffset method, missing page reset

--- my-app/src/DropdownBox.js
+++ my-app/src/DropdownBox.js
@@ -1,9 +1,19 @@
+import { useState } from "react";
+
 export const DropdownBox = (props) => {
-  const { setLimit } = props;
+  const { limit, setLimit, setOffset } = props;
+  let [page, setPage] = useState(0);
 
   const handlePagination = (e) => {
     e.preventDefault();
     setLimit(e.target.value);
+  };
+
+  const handleOffset = (e, change) => {
+    e.preventDefault();
+    page = page + change;
+    setPage(page);
+    setOffset(page * limit);
   };
   return (
     <>
@@ -14,6 +24,9 @@ export const DropdownBox = (props) => {
         <option value={50}>50</option>
         <option value={100}>100</option>
       </select>
+      <button onClick={(e) => handleOffset(e, -1)}>previous page</button>{" "}
+      <button onClick={(e) => handleOffset(e, 1)}>next page</button>
+      <p>Page {page + 1}</p>
     </>
   );
 };
