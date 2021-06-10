8b211f067f05f63320779b4fef967ad96c43c9f7
Giancarlo Danves <gdanves@gmail.com>
2021-06-09 21:26:53 -0300

    receving setStates and loading as props to control paginations and buttons

--- my-app/src/DropdownBox.js
+++ my-app/src/DropdownBox.js
@@ -1,8 +1,6 @@
-import { useState } from "react";
-
 export const DropdownBox = (props) => {
-  const { limit, setLimit, setOffset } = props;
-  let [page, setPage] = useState(0);
+  const { limit, setLimit, setOffset, setPage, loading } = props;
+  let { page } = props;
 
   const handlePagination = (e) => {
     e.preventDefault();
@@ -18,14 +16,27 @@ export const DropdownBox = (props) => {
   return (
     <>
       <p>Show: </p>
-      <select onChange={(e) => handlePagination(e)}>
+      <select
+        disabled={loading ? true : false}
+        onChange={(e) => handlePagination(e)}
+      >
         <option value={10}>10</option>
         <option value={25}>25</option>
         <option value={50}>50</option>
         <option value={100}>100</option>
       </select>
-      <button onClick={(e) => handleOffset(e, -1)}>previous page</button>{" "}
-      <button onClick={(e) => handleOffset(e, 1)}>next page</button>
+      <button
+        disabled={page === 0 || loading ? true : false}
+        onClick={(e) => handleOffset(e, -1)}
+      >
+        previous page
+      </button>
+      <button
+        disabled={loading ? true : false}
+        onClick={(e) => handleOffset(e, 1)}
+      >
+        next page
+      </button>
       <p>Page {page + 1}</p>
     </>
   );
