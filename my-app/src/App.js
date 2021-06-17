a3ac5cad85609df4bf867eb2412cf42da043a91e
Giancarlo Danves <gdanves@gmail.com>
2021-06-16 23:02:21 -0300

    adjusting home page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,18 @@
-import { useState } from "react";
-import { Pokemons } from "./Pokemons";
-import { handleLoadAndError } from "./HandleLoadAndError";
 import { PaginationController } from "./PaginationController";
-import { getUrl } from "./getUrl";
-import { Formik, Form, Field } from "formik";
+import { Pokemons } from "./Pokemons";
+import { useContext } from "react";
+import { handleLoadAndError } from "./HandleLoadAndError";
 import { useQuery } from "react-query";
-const defaultUrl = process.env.REACT_APP_DEFAULT_URL;
-const poketypesUrl = process.env.REACT_APP_POKETYPES_URL;
+import { Context } from "./Context";
+import { Link } from "react-router-dom";
 
 function App() {
-  let [url, setUrl] = useState(defaultUrl);
-  const [limit, setLimit] = useState(10);
-  const [offset, setOffset] = useState(0);
-
+  const { url, limit, setLimit, offset, setOffset } = useContext(Context);
   const getLimit = () => {
     return `&limit=${limit}`;
   };
+
+  console.log(url);
 
   const getOffset = () => {
     return `&offset=${offset}`;
@@ -28,122 +25,19 @@ function App() {
     refetch,
   } = useQuery(`${url}${getLimit()}${getOffset()}`, { retryDelay: 1000 });
 
-  const {
-    isLoading: typelistIsLoading,
-    isError: typelistHaserror,
-    data: typelistData,
-    refetch: refetchTypelist,
-  } = useQuery(poketypesUrl, { retryDelay: 1000 });
-
-  const onSubmit = (values) => {
-    setOffset(0);
-    url = defaultUrl;
-    const filteredUrl = getUrl(values, url);
-    setUrl(filteredUrl);
-  };
-
-  const resetUrl = () => {
-    setUrl(defaultUrl);
-    setOffset(0);
-  };
-
   return (
     <>
-      <Formik
-        onSubmit={onSubmit}
-        initialValues={{
-          heights: [],
-          weights: [],
-          search: "",
-          type: [],
-        }}
-      >
-        <Form>
-          <div>
-            <Field name="search" type="text" placeholder="Search by name" />
-          </div>
-          <div>
-            <p>Height</p>
-            <label>
-              <Field type="checkbox" name="heights" value="1" />1
-            </label>
-
-            <label>
-              <Field type="checkbox" name="heights" value="2" />2
-            </label>
-
-            <label>
-              <Field type="checkbox" name="heights" value="3" />3
-            </label>
-
-            <label>
-              <Field type="checkbox" name="heights" value="4" />4
-            </label>
-
-            <label>
-              <Field type="checkbox" name="heights" value="5" />5
-            </label>
-          </div>
-          <div>
-            <p>Weight</p>
-            <label>
-              <Field type="checkbox" name="weights" value="1" />1
-            </label>
-
-            <label>
-              <Field type="checkbox" name="weights" value="2" />2
-            </label>
-
-            <label>
-              <Field type="checkbox" name="weights" value="3" />3
-            </label>
-
-            <label>
-              <Field type="checkbox" name="weights" value="4" />4
-            </label>
-
-            <label>
-              <Field type="checkbox" name="weights" value="5" />5
-            </label>
-          </div>
-
-          <div>
-            <p>Types</p>
-            {handleLoadAndError(typelistIsLoading, typelistHaserror) ||
-              typelistData.results.map((pokeType) => {
-                return (
-                  <>
-                    <label>
-                      <Field
-                        type="checkbox"
-                        name="type"
-                        value={pokeType.name}
-                      />
-                      {pokeType.name}
-                    </label>
-                  </>
-                );
-              })}
-          </div>
-          {error ? (
-            <button onClick={() => refetchTypelist()}>Try Again</button>
-          ) : (
-            <></>
-          )}
-          <button type="reset" onClick={resetUrl}>
-            Clear filters
-          </button>
-          <button type="submit">Search</button>
-          <PaginationController
-            limit={limit}
-            setLimit={setLimit}
-            setOffset={setOffset}
-            offset={offset}
-            loading={isLoading}
-            count={pokemons?.count}
-          />
-        </Form>
-      </Formik>
+      <Link to="/filters">
+        <button>Filters</button>
+      </Link>
+      <PaginationController
+        limit={limit}
+        setLimit={setLimit}
+        setOffset={setOffset}
+        offset={offset}
+        loading={isLoading}
+        count={pokemons?.count}
+      />
       <ul>
         {handleLoadAndError(isLoading, error) || (
           <Pokemons pokemons={pokemons.results} />
