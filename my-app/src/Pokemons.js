71346b3f4eece0bfd7a9ce913709e02cf7101773
Giancarlo Danves <gdanves@gmail.com>
2021-06-21 13:54:50 -0300

    styling elements

--- my-app/src/Pokemons.js
+++ my-app/src/Pokemons.js
@@ -1,3 +1,6 @@
+import addIcon from "./img/add-icon.svg";
+import favoriteIcon from "./img/favorite-icon.svg";
+
 export function Pokemons(props) {
   const { pokemons } = props;
 
@@ -11,7 +14,7 @@ export function Pokemons(props) {
     return n;
   };
   return (
-    <section className="flex space-x-5">
+    <section className="flex space-x-5 space-y-5 flex-wrap">
       {pokemons.length === 0 ? (
         <p>
           No Pokemons matches this criterias, change the filters and try again
@@ -28,23 +31,27 @@ export function Pokemons(props) {
               <li
                 className={`w-full bg-gradient-to-br from-${type[0]} to-transparent h-full flex flex-col items-center justify-end`}
               >
-                <p className="text-subtitle self-start">
-                  {"#" + getThreeDigits(id)}
-                </p>
-                <img className="pokemon-avatar" src={picture} alt={name} />
-                <h2>{name}</h2>
+                <div className="flex items-stretch">
+                  <p className="text-subtitle self-start font-bold">
+                    {"#" + getThreeDigits(id)}
+                  </p>
+                  <img className="" src={addIcon} alt="add Pokemon" />
+                  <img src={favoriteIcon} alt="favorite Pokemon" />
+                </div>
+                <img className="h-avatar w-avatar" src={picture} alt={name} />
+                <h2 className="font-nunito text-body-01 font-bold">{name}</h2>
                 <div className=" flex bg-white h-1/6 w-full space-x-1">
                   {type.map((poketype, i) => {
                     if (i === 1) {
                       return (
                         <p
-                          className={`h-4 text-subtitle self-center rounded border border-${poketype}`}
+                          className={`h-4 text-subtitle leading-subtitle self-center rounded border border-${poketype}`}
                         >{`${poketype}`}</p>
                       );
                     }
                     return (
                       <p
-                        className={`h-4 text-subtitle self-center border rounded ml-1 border-${poketype}`}
+                        className={`h-4 text-subtitle self-center leading-subtitle border rounded ml-1 border-${poketype}`}
                       >
                         {poketype}
                       </p>
