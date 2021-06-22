eec4b7fb328007c17439cd2ee4f1354e29af3cb8
Giancarlo Danves <gdanves@gmail.com>
2021-06-22 17:25:37 -0300

    styling home

--- my-app/src/Pokemons.js
+++ my-app/src/Pokemons.js
@@ -14,7 +14,7 @@ export function Pokemons(props) {
     return n;
   };
   return (
-    <section className="flex space-x-5 space-y-5 flex-wrap">
+    <div className="flex flex-wrap mx-auto max-w-screen-2xl">
       {pokemons.length === 0 ? (
         <p>
           No Pokemons matches this criterias, change the filters and try again
@@ -24,45 +24,56 @@ export function Pokemons(props) {
           const { picture, name, id, type } = pokemon;
 
           return (
-            <article
-              className={`border border-${type[0]} rounded-md h-card w-card`}
-              key={id}
-            >
+            <div className={`w-6/12 md:w-4/12 lg:w-3/12 xl:w-2/12 p-1`}>
               <li
-                className={`w-full bg-gradient-to-br from-${type[0]} to-transparent h-full flex flex-col items-center justify-end`}
+                className={`border border-${type[0]} rounded-md bg-gradient-to-br from-${type[0]} to-transparent flex flex-col items-center overflow-hidden`}
               >
-                <div className="flex items-stretch">
-                  <p className="text-subtitle self-start font-bold">
+                <div className="flex justify-between w-full">
+                  <p className="text-sm font-bold">
                     {"#" + getThreeDigits(id)}
                   </p>
-                  <img className="" src={addIcon} alt="add Pokemon" />
-                  <img src={favoriteIcon} alt="favorite Pokemon" />
+                  <div className="flex">
+                    <img
+                      className="h-2/3 m-1"
+                      src={addIcon}
+                      alt="add Pokemon"
+                    />
+                    <img
+                      className="h-2/3 m-1"
+                      src={favoriteIcon}
+                      alt="favorite Pokemon"
+                    />
+                  </div>
                 </div>
-                <img className="h-avatar w-avatar" src={picture} alt={name} />
-                <h2 className="font-nunito text-body-01 font-bold">{name}</h2>
-                <div className=" flex bg-white h-1/6 w-full space-x-1">
-                  {type.map((poketype, i) => {
-                    if (i === 1) {
+                <img className="h-avatar" src={picture} alt={name} />
+                <h2 className="font-nunito text-body-01 capitalize font-bold">
+                  {name}
+                </h2>
+                <div className="bg-white h-8 w-full space-x-1">
+                  <div className="h-full flex content-center">
+                    {type.map((poketype, i) => {
+                      if (i === 1) {
+                        return (
+                          <p
+                            className={`flex text-subtitle leading-subtitle items-center self-center rounded border border-${poketype}`}
+                          >{`${poketype}`}</p>
+                        );
+                      }
                       return (
                         <p
-                          className={`h-4 text-subtitle leading-subtitle self-center rounded border border-${poketype}`}
-                        >{`${poketype}`}</p>
+                          className={`flex text-subtitle self-center leading-subtitle items-center border rounded ml-1 border-${poketype}`}
+                        >
+                          {poketype}
+                        </p>
                       );
-                    }
-                    return (
-                      <p
-                        className={`h-4 text-subtitle self-center leading-subtitle border rounded ml-1 border-${poketype}`}
-                      >
-                        {poketype}
-                      </p>
-                    );
-                  })}
+                    })}
+                  </div>
                 </div>
               </li>
-            </article>
+            </div>
           );
         })
       )}
-    </section>
+    </div>
   );
 }
