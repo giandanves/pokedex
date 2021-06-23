4e39dfb4331eeed4befe7eea15d057495b103778
Giancarlo Danves <gdanves@gmail.com>
2021-06-23 09:49:12 -0300

    changing labels from weight and height filters

--- my-app/src/Weight.js
+++ my-app/src/Weight.js
@@ -1,13 +1,14 @@
 import { Field } from "formik";
 
 export const Weight = () => {
+  const labels = ["Extra Light", "Light", "Medium", "Large", "Extra Large"];
   return (
     <div>
       <p>Weight</p>
       {[1, 2, 3, 4, 5].map((vl) => (
         <label>
           <Field type="checkbox" name="weight" value={`${vl}`} key={vl} />
-          {vl}
+          {labels[vl - 1]}
         </label>
       ))}
     </div>
