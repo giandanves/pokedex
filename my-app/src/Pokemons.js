7b505078c4f4da45654da13bba2633e10ece7ebe
Giancarlo Danves <gdanves@gmail.com>
2021-06-26 15:09:08 -0300

    applying max screen class

--- my-app/src/Pokemons.js
+++ my-app/src/Pokemons.js
@@ -14,7 +14,7 @@ export function Pokemons(props) {
     return n;
   };
   return (
-    <div className="flex flex-wrap mx-auto max-w-screen-2xl">
+    <div className="flex flex-wrap mx-auto max-w-fullscreen">
       {pokemons.length === 0 ? (
         <p>
           No Pokemons matches this criterias, change the filters and try again
