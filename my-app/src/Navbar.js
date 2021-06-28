7a412b5068edc9eb4076e215f22d953c3c35730e
Giancarlo Danves <gdanves@gmail.com>
2021-06-27 23:18:55 -0300

    creating border that show the actual page

--- my-app/src/Navbar.js
+++ my-app/src/Navbar.js
@@ -5,25 +5,37 @@ import signin from "./img/signin.svg";
 
 export const Navbar = () => {
   return (
-    <div className="hidden sm:flex justify-between border-b border-lightgrey h-16 w-full max-w-fullscreen py-6 px-5">
+    <div className="hidden sm:flex justify-between border-b border-lightgrey h-16 w-full max-w-fullscreen pt-5 pb-6 px-6">
       <div className="flex">
-        <button className="flex h-6 pr-3 items-center">
+        <button className="flex h-12 pr-3 items-start">
           <img src={pokedex} alt="pokedexIcon" className="mr-2" />
-          <h2 className="text-base text-black font-bold">Pokedex</h2>
+          <div>
+            <h2 className="text-base text-black font-bold">Pokedex</h2>
+            <div className="h-1 w-full bg-primary rounded-lg"></div>
+          </div>
         </button>
-        <button className="flex h-6 px-3 items-center">
+        <button className="flex h-12 px-3 items-start">
           <img src={teams} alt="teamsIcon" className="mr-2" />
-          <h2 className="text-base text-black font-bold">Teams</h2>
+          <div>
+            <h2 className="text-base text-black font-bold">Teams</h2>
+            <div className="h-1 w-full bg-white rounded-lg"></div>
+          </div>
         </button>
-        <button className="flex h-6 px-3 items-center">
+        <button className="flex h-12 px-3 items-start">
           <img src={favpage} alt="favorites" className="mr-2" />
-          <h2 className="text-base text-black font-bold">Favorites</h2>
+          <div>
+            <h2 className="text-base text-black font-bold">Favorites</h2>
+            <div className="h-1 w-full bg-white rounded-lg"></div>
+          </div>
         </button>
       </div>
 
-      <button className="flex h-6 px-3 items-center">
+      <button className="flex h-12 px-3 items-start">
         <img src={signin} alt="sign in" className="mr-3" />
-        <h2 className="text-base text-black font-bold">Sign in</h2>
+        <div>
+          <h2 className="text-base text-black font-bold">Sign in</h2>
+          <div className="h-1 w-full bg-white rounded-lg"></div>
+        </div>
       </button>
     </div>
   );
