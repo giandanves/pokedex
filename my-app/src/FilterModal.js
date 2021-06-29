76dd3d86760a2c5eccaf021c12b894b5acd02190
Giancarlo Danves <gdanves@gmail.com>
2021-06-29 11:27:12 -0300

    applying elements from filter page to filter modal

--- my-app/src/FilterModal.js
+++ my-app/src/FilterModal.js
@@ -1,9 +1,42 @@
+import { Formik, Form } from "formik";
+import { Type } from "./TypeFilter";
+import { Moves } from "./Moves";
+import { Weight } from "./Weight";
+import { Height } from "./Height";
+import { Abilities } from "./Abilities";
+import { FilterContext } from "./FilterContext";
+import { useContext } from "react";
 import ReactModal from "react-modal";
 
 export const FilterModal = () => {
+  let {
+    setOffset,
+    filter,
+    setFilter,
+    filterModalIsOpen,
+    setFilterModalIsOpen,
+  } = useContext(FilterContext);
+
+  const onSubmit = (values) => {
+    setOffset(0);
+    setFilter((prev) => (prev = values));
+    setFilterModalIsOpen(false);
+  };
+
+  const resetFilters = () => {
+    setFilter({
+      height: [],
+      weight: [],
+      type: [],
+      ability: [],
+      move: [],
+      search: "",
+    });
+    setOffset(0);
+  };
   return (
     <ReactModal
-      isOpen={true}
+      isOpen={filterModalIsOpen}
       bodyOpenClassName="body-open"
       style={{
         overlay: {
@@ -11,6 +44,7 @@ export const FilterModal = () => {
           backgroundColor: "#2B314060",
         },
         content: {
+          padding: "20px 32px 0px 32px",
           opacity: "1",
           backgroundColor: "white",
           height: "100%",
@@ -24,7 +58,26 @@ export const FilterModal = () => {
       }}
     >
       <div className="h-full w-full bg-white bg-opacity-1">
-        <p>Filter</p>
+        <p>Filters</p>
+        <Formik
+          enableReinitialize={true}
+          onSubmit={onSubmit}
+          initialValues={filter}
+        >
+          <Form>
+            <button type="reset" onClick={resetFilters}>
+              Reset
+            </button>
+            <button type="submit">Apply</button>
+
+            <Height />
+            <Weight />
+
+            <Type />
+            <Moves filter={filter} />
+            <Abilities filter={filter} />
+          </Form>
+        </Formik>
       </div>
     </ReactModal>
   );
