c887030b5029ee5c9a1b97f77cdddbde353b879a
Giancarlo Danves <gdanves@gmail.com>
2021-06-29 16:27:42 -0300

    styling weight component with same rules from height

--- my-app/src/Weight.js
+++ my-app/src/Weight.js
@@ -1,13 +1,24 @@
-import { Field } from "formik";
+import { Field, useFormikContext } from "formik";
 
 export const Weight = () => {
   const labels = ["Extra Light", "Light", "Medium", "Large", "Extra Large"];
+  const { values } = useFormikContext();
   return (
-    <div>
-      <p>Weight</p>
+    <div className="pt-9">
+      <p className="pb-4 text-base font-bold text-black">Weight</p>
       {[1, 2, 3, 4, 5].map((vl) => (
-        <label>
-          <Field type="checkbox" name="weight" value={`${vl}`} key={vl} />
+        <label
+          className={`bg-${
+            values.weight.includes(vl.toString()) && "lightblue"
+          } label-for-check py-2 px-3 mr-2 text-black font-bold text-xs border border-black-300 rounded`}
+        >
+          <Field
+            type="checkbox"
+            name="weight"
+            value={`${vl}`}
+            key={vl}
+            className="hidden"
+          />
           {labels[vl - 1]}
         </label>
       ))}
