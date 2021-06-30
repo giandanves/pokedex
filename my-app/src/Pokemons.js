eb95709360d776c8cf64ee32bff196e016b7e932
Giancarlo Danves <gdanves@gmail.com>
2021-06-30 20:29:43 -0300

    i don't know where to place shadow

--- my-app/src/Pokemons.js
+++ my-app/src/Pokemons.js
@@ -14,7 +14,7 @@ export function Pokemons(props) {
     return n;
   };
   return (
-    <div className="flex flex-wrap mx-auto max-w-fullscreen max-h-pokemoncontainer sm:max-h-full overflow-auto">
+    <div className="shadow-shadow flex flex-wrap mx-auto max-w-fullscreen max-h-pokemoncontainer sm:max-h-full overflow-auto">
       {pokemons.length === 0 ? (
         <p>
           No Pokemons matches this criterias, change the filters and try again
@@ -26,13 +26,13 @@ export function Pokemons(props) {
           return (
             <div
               key={id}
-              className={`w-6/12 py-2 pr-4 sm:w-4/12 sm:pr-4 md:w-3/12 md:py-3 md:pr-8 lg:w-2/12`}
+              className={`shadow-shadow w-6/12 py-2 pr-4 sm:w-4/12 sm:pr-4 md:w-3/12 md:py-3 md:pr-8 lg:w-2/12`}
             >
               <li
-                className={`border border-${type[0]} rounded-md overflow-hidden`}
+                className={`shadow-shadow border border-${type[0]} rounded-md overflow-hidden`}
               >
                 <div
-                  className={`w-full bg-${type[0]} bg-opacity-20 bg-gradient-to-br from-${type[0]} to-transparent rounded-br-2xl flex flex-col items-center p-3`}
+                  className={`shadow-shadow w-full bg-${type[0]} bg-opacity-20 bg-gradient-to-br from-${type[0]} to-transparent rounded-br-2xl flex flex-col items-center p-3`}
                 >
                   <div className="flex justify-between items-center w-full">
                     <p className="text-subtitle font-bold text-black">
