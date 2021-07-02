74f26d9175b451c05942f48bd169a000b4452fe3
Giancarlo Danves <gdanves@gmail.com>
2021-07-02 10:58:40 -0300

    styling navbar

--- my-app/src/Navbar.js
+++ my-app/src/Navbar.js
@@ -48,76 +48,82 @@ export const Navbar = () => {
   return (
     <div className="hidden sm:flex justify-between border-b border-lightgrey h-16 w-full max-w-fullscreen pt-5 pb-6 px-6">
       <div className="flex">
-        <button
-          className="flex h-12 pr-3 items-start"
-          onClick={() => goToPokedex()}
-        >
-          <img
-            src={pokedex}
-            alt="pokedexIcon"
-            className={`mr-2 ${isOnPokedex && "filter-primary"}`}
-          />
-          <div>
-            <h2
-              className={`text-base text-${
-                isOnPokedex ? "primary" : "black"
-              } font-bold`}
-            >
-              Pokedex
-            </h2>
-            <div
-              className={`h-0.5 w-full bg-${
-                isOnPokedex ? "primary" : "white"
-              } rounded-lg`}
-            ></div>
+        <button className="flex h-12 pr-3" onClick={() => goToPokedex()}>
+          <div className="flex">
+            <img
+              src={pokedex}
+              alt="pokedexIcon"
+              className={`self-end mr-2 ${isOnPokedex && "filter-primary"}`}
+            />
+            <div className="self-end">
+              <h2
+                className={`text-base text-${
+                  isOnPokedex ? "primary" : "black"
+                } font-bold`}
+              >
+                Pokedex
+              </h2>
+              {isOnPokedex && (
+                <div
+                  className="h-0.5 w-full bg-primary
+              } rounded-lg"
+                ></div>
+              )}
+            </div>
           </div>
         </button>
         <button
           className="flex h-12 px-3 items-start"
           onClick={() => goToTeams()}
         >
-          <img
-            src={teams}
-            alt="teamsIcon"
-            className={`mr-2 ${teamsIsOpen && "filter-primary"}`}
-          />
-          <div>
-            <h2
-              className={`text-base text-${
-                teamsIsOpen ? "primary" : "black"
-              } font-bold`}
-            >
-              Teams
-            </h2>
-            <div
-              className={`h-0.5 w-full bg-${
-                teamsIsOpen ? "primary" : "white"
-              } rounded-lg`}
-            ></div>
+          <div className="flex">
+            <img
+              src={teams}
+              alt="teamsIcon"
+              className={`self-end mr-2 ${teamsIsOpen && "filter-primary"}`}
+            />
+            <div className="self-end">
+              <h2
+                className={`text-base text-${
+                  teamsIsOpen ? "primary" : "black"
+                } font-bold`}
+              >
+                Teams
+              </h2>
+              {teamsIsOpen && (
+                <div
+                  className="h-0.5 w-full bg-primary
+              } rounded-lg"
+                ></div>
+              )}
+            </div>
           </div>
         </button>
         <button
           className="flex h-12 px-3 items-start"
           onClick={() => goToFavorites()}
         >
-          <img
-            src={favpage}
-            alt="favorites"
-            className={`mr-2 ${favoritesIsOpen && "filter-primary"}`}
-          />
-          <div>
-            <h2
-              className={`text-base text-${
-                favoritesIsOpen ? "primary" : "black"
-              } font-bold`}
-            >
-              Favorites
-            </h2>
-            <div
-              className={`h-0.5 w-full bg-${
-                favoritesIsOpen ? "primary" : "white"
-              } rounded-lg`}
-            ></div>
+          <div className="flex">
+            <img
+              src={favpage}
+              alt="favorites"
+              className={`self-end mr-2 ${favoritesIsOpen && "filter-primary"}`}
+            />
+            <div className="self-end">
+              <h2
+                className={`text-base text-${
+                  favoritesIsOpen ? "primary" : "black"
+                } font-bold`}
+              >
+                Favorites
+              </h2>
+              {favoritesIsOpen && (
+                <div
+                  className="h-0.5 w-full bg-primary
+              } rounded-lg"
+                ></div>
+              )}
+            </div>
           </div>
         </button>
       </div>
@@ -126,24 +132,27 @@ export const Navbar = () => {
         className="flex h-12 px-3 items-start"
         onClick={() => goToSignIn()}
       >
-        <img
-          src={signin}
-          alt="sign in"
-          className={`mr-3 ${signInIsOpen && "filter-primary"}`}
-        />
-        <div>
-          <h2
-            className={`text-base text-${
-              signInIsOpen ? "primary" : "black"
-            } font-bold`}
-          >
-            Sign in
-          </h2>
-          <div
-            className={`h-0.5 w-full bg-${
-              signInIsOpen ? "primary" : "white"
-            } rounded-lg`}
-          ></div>
+        <div className="flex">
+          <img
+            src={signin}
+            alt="sign in"
+            className={`self-center mr-3 ${signInIsOpen && "filter-primary"}`}
+          />
+          <div className="self-end">
+            <h2
+              className={`text-base text-${
+                signInIsOpen ? "primary" : "black"
+              } font-bold`}
+            >
+              Sign in
+            </h2>
+            {signInIsOpen && (
+              <div
+                className="h-0.5 w-full bg-primary
+              } rounded-lg"
+              ></div>
+            )}
+          </div>
         </div>
       </button>
     </div>
