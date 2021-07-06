f3b078731d229bfc9da8bade0831e2bf7a77539c
Giancarlo Danves <gdanves@gmail.com>
2021-07-06 15:44:42 -0300

    creating const that handle status of checkbox

--- my-app/src/Checkbox.js
+++ my-app/src/Checkbox.js
@@ -5,12 +5,14 @@ import checkedImg from "./img/checked.svg";
 const Checkbox = (props) => {
   const { label, name, value, values } = props;
 
+  const isChecked = values.includes(value.toString());
+
   return (
     <label
       className={classNames(
         "flex whitespace-nowrap capitalize h-6 py-1 px-2 mr-2 mb-2  text-black font-bold text-subtitle leading-subtitle border border-black-300 rounded",
         {
-          "bg-lightblue border-primary": values.includes(value.toString()),
+          "bg-lightblue border-primary": isChecked,
         }
       )}
     >
@@ -18,7 +20,7 @@ const Checkbox = (props) => {
         src={checkedImg}
         alt="checked"
         className={classNames("opacity-0", {
-          "opacity-100": values.includes(value.toString()),
+          "opacity-100": isChecked,
         })}
       />
       <Field
