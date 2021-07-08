763098bf64c0c17586eb9bd02963973696471654
Giancarlo Danves <gdanves@gmail.com>
2021-07-08 16:56:19 -0300

    loading component

--- my-app/src/loading.js
+++ my-app/src/loading.js
@@ -1,5 +1,21 @@
 const Loading = () => {
-  return <p>loading...</p>;
+  return (
+    <div className="flex h-full items-center">
+      <div className="loading-container mx-auto">
+        <svg class="circular-loader" viewBox="25 25 50 50">
+          <circle
+            class="loader-path"
+            cx="50"
+            cy="50"
+            r="20"
+            fill="none"
+            stroke="#70c542"
+            stroke-width="2"
+          />
+        </svg>
+      </div>
+    </div>
+  );
 };
 
 export default Loading;
