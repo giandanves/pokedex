7643ebdbc0b3dce76b2e38953f195f1fbc6f5583
Giancarlo Danves <gdanves@gmail.com>
2021-07-09 17:01:05 -0300

    empty state

--- my-app/src/empty.js
+++ my-app/src/empty.js
@@ -2,11 +2,15 @@ import emptyIcon from "./img/empty-state-icon.svg";
 
 const Empty = () => {
   return (
-    <div>
+    <div className="flex flex-col mx-auto items-center">
       <img src={emptyIcon} alt="Empty search" />
       <div className="py-6">
-        <h2 className="text-base font-bold">Your search returned 0 results</h2>
-        <p>Try to be be less specific and try again</p>
+        <h2 className="text-base font-bold text-center">
+          Your search returned 0 results
+        </h2>
+        <p className="py-2 text-center">
+          Try to be less specific and try again
+        </p>
       </div>
     </div>
   );
