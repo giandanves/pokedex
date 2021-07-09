c0dae5cba479fdb519daab6e20be4b2e14e13f47
Giancarlo Danves <gdanves@gmail.com>
2021-07-09 16:42:20 -0300

    creating component

--- my-app/src/empty.js
+++ my-app/src/empty.js
@@ -1,7 +1,15 @@
 import emptyIcon from "./img/empty-state-icon.svg";
 
 const Empty = () => {
-  return <p>Empty...</p>;
+  return (
+    <div>
+      <img src={emptyIcon} alt="Empty search" />
+      <div className="py-6">
+        <h2 className="text-base font-bold">Your search returned 0 results</h2>
+        <p>Try to be be less specific and try again</p>
+      </div>
+    </div>
+  );
 };
 
 export default Empty;
