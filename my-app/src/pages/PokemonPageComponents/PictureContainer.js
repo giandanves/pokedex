b2cb355426d6027d045fcc5f7d808e760c052cd3
Giancarlo Danves <gdanves@gmail.com>
2021-07-16 19:41:59 -0300

    using z-index in picture to style it

--- my-app/src/pages/PokemonPageComponents/PictureContainer.js
+++ my-app/src/pages/PokemonPageComponents/PictureContainer.js
@@ -7,13 +7,13 @@ const PictureContainer = ({ type, picture }) => {
     <div
       className={classNames(
         getBackground(type),
-        "to-transparent w-full aspect-w-1 aspect-h-1 sm:aspect-none sm:w-5/12 bg-opacity-20 bg-gradient-to-br flex items-center justify-center sm:p-2"
+        "to-transparent w-full aspect-w-1 aspect-h-1  sm:aspect-none sm:w-5/12 bg-opacity-20 bg-gradient-to-br flex items-center justify-center sm:p-2"
       )}
     >
       <MobileHeader />
-      <div className="w-full flex items-center sm:max-w-sm">
+      <div className="w-full  flex items-center sm:max-w-sm">
         <img
-          className="h-1/2 w-1/2 sm:h-full sm:w-full mx-auto"
+          className="h-1/2 w-1/2 relative z-10 sm:h-full sm:w-full mx-auto"
           src={picture}
           alt="name"
         />
