aa17a47ba47ba90c9a6e0f093bff9a3d19655a93
Giancarlo Danves <gdanves@gmail.com>
2021-07-16 19:41:11 -0300

    making header responsive

--- my-app/src/pages/PokemonPageComponents/Header.js
+++ my-app/src/pages/PokemonPageComponents/Header.js
@@ -2,7 +2,7 @@ import numberToThreeDigits from "../../numberToThreeDigits";
 
 const Header = ({ name, id }) => {
   return (
-    <div>
+    <div className="flex justify-between items-center w-full sm:block">
       <h1 className="capitalize font-bold text-black text-2xl">{name}</h1>
       <p className="text-black-500 font-bold text-sm">
         {"#" + numberToThreeDigits(id)}
