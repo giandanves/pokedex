e0b9c2595fd3211aff1c877fd6c0744e17f38867
Giancarlo Danves <gdanves@gmail.com>
2021-07-16 17:16:53 -0300

    rendering mobile header

--- my-app/src/pages/PokemonPageComponents/PictureContainer.js
+++ my-app/src/pages/PokemonPageComponents/PictureContainer.js
@@ -1,5 +1,6 @@
 import getBackground from "../../getBackground";
 import classNames from "classnames";
+import MobileHeader from "./MobileHeader";
 
 const PictureContainer = ({ type, picture }) => {
   return (
@@ -9,7 +10,8 @@ const PictureContainer = ({ type, picture }) => {
         "to-transparent w-full aspect-w-1 aspect-h-1 sm:aspect-none sm:w-5/12 bg-opacity-20 bg-gradient-to-br flex items-center justify-center sm:p-2"
       )}
     >
-      <div className="w-full flex items-center max-w-sm">
+      <MobileHeader />
+      <div className="w-full flex items-center sm:max-w-sm">
         <img
           className="h-1/2 w-1/2 sm:h-full sm:w-full mx-auto"
           src={picture}
