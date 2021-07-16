3019218678af4096f93f588d7b2e60a265dd8c1d
Giancarlo Danves <gdanves@gmail.com>
2021-07-16 19:41:45 -0300

    making evolutions container responsive

--- my-app/src/pages/PokemonPageComponents/EvolutionsContainer.js
+++ my-app/src/pages/PokemonPageComponents/EvolutionsContainer.js
@@ -1,9 +1,9 @@
 function EvolutionsContainer() {
   return (
-    <article className="pl-8 w-7/12">
+    <article className="sm:pl-8 sm:w-7/12">
       <h2 className="pt-6 pb-4 text-base font-bold text-black">Evolutions</h2>
       <div className="w-full">
-        <p className="w-full text-center pt-10">Placeholder</p>
+        <p className="w-full sm:text-center pt-10 pb-4">Placeholder</p>
       </div>
     </article>
   );
