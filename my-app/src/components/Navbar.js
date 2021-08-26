fbaa53078c1eab2b17bc1aba612c2770e4e4b9f2
Giancarlo Danves <gdanves@gmail.com>
2021-08-26 15:20:35 -0300

    rendering user on navbar when is logged in

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -4,8 +4,12 @@ import teams from "../img/teams.svg";
 import signin from "../img/signin.svg";
 import { Link } from "react-router-dom";
 import { NavbarIcon } from "./NavbarIcon";
+import { AuthContext } from "../Authentication";
+import { useContext } from "react";
+import { signOut } from "../authenticationFunctions";
 
 export const Navbar = () => {
+  const { logged, user, setUser } = useContext(AuthContext);
   return (
     <div className="hidden sm:flex border-b  border-lightgrey h-16 w-full pb-6 px-6">
       <div className="flex justify-between w-full max-w-fullscreen mx-auto">
@@ -20,9 +24,24 @@ export const Navbar = () => {
             <NavbarIcon img={favorites} title="Favorites" path={"/favorites"} />
           </Link>
         </div>
-        <Link to="/signin">
-          <NavbarIcon img={signin} title="Sign in" path={"/signin"} />
-        </Link>
+        {logged ? (
+          <div>
+            <NavbarIcon img={signin} title={user?.username} path={"/signin"} />
+            <p
+              className="text-xs text-danger text-right"
+              onClick={() => {
+                signOut();
+                setUser("logging out");
+              }}
+            >
+              Logout
+            </p>
+          </div>
+        ) : (
+          <Link to="/signin">
+            <NavbarIcon img={signin} title="Sign in" path={"/signin"} />
+          </Link>
+        )}
       </div>
     </div>
   );
