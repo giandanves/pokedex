fa42717cfe26f241737b44f32ef672f92d14dfd9
Giancarlo Danves <gdanves@gmail.com>
2021-08-27 15:34:52 -0300

    rendering errors

--- my-app/src/pages/SignInPage.js
+++ my-app/src/pages/SignInPage.js
@@ -1,11 +1,10 @@
 import { Formik, Form, Field } from "formik";
 import { useContext } from "react";
 import Button from "../components/Button";
-import { signIn } from "../authenticationFunctions";
 import { AuthContext } from "../Authentication";
 
 export const SignInPage = ({ setStep }) => {
-  const { setUser } = useContext(AuthContext);
+  const { signIn, signInErrorMessage } = useContext(AuthContext);
   return (
     <div className="py-6">
       <h2 className="text-base font-bold text-center">Sign in</h2>
@@ -18,7 +17,6 @@ export const SignInPage = ({ setStep }) => {
         onSubmit={(values) => {
           console.dir(values);
           signIn(values.email, values.password);
-          setUser("logging in");
         }}
       >
         <Form>
@@ -36,12 +34,15 @@ export const SignInPage = ({ setStep }) => {
             {({ field, form, meta }) => (
               <input
                 type="text"
-                className="text-body-02 w-full h-12 px-2 my-6 border border-gray rounded-lg"
+                className="text-body-02 w-full h-12 px-2 mt-6 border border-gray rounded-lg"
                 placeholder={"Password"}
                 {...field}
               />
             )}
           </Field>
+          {signInErrorMessage && (
+            <p className="text-danger text-xs my-2">{signInErrorMessage}</p>
+          )}
           <div className="w-full flex flex-col space-y-2">
             <Button
               textColor={"white"}
--- my-app/src/pages/SignUpPage.js
+++ my-app/src/pages/SignUpPage.js
@@ -1,11 +1,10 @@
 import { Formik, Form, Field } from "formik";
 import { useContext } from "react";
 import Button from "../components/Button";
-import { createAccount } from "../authenticationFunctions";
 import { AuthContext } from "../Authentication";
 
 const SignUpPage = ({ setStep }) => {
-  const { setUser } = useContext(AuthContext);
+  const { createAccount, signUpErrorMessages } = useContext(AuthContext);
   return (
     <div className="py-6">
       <h2 className="text-base font-bold text-center">Sign Up</h2>
@@ -19,7 +18,6 @@ const SignUpPage = ({ setStep }) => {
         onSubmit={(values) => {
           console.dir(values);
           createAccount(values.name, values.email, values.password);
-          setUser("signing up");
         }}
       >
         <Form>
@@ -33,6 +31,11 @@ const SignUpPage = ({ setStep }) => {
               />
             )}
           </Field>
+          {signUpErrorMessages.name && (
+            <p className="text-danger text-xs my-2">
+              {signUpErrorMessages.name}
+            </p>
+          )}
           <Field name="email">
             {({ field, form, meta }) => (
               <input
@@ -43,6 +46,11 @@ const SignUpPage = ({ setStep }) => {
               />
             )}
           </Field>
+          {signUpErrorMessages.email && (
+            <p className="text-danger text-xs my-2">
+              {signUpErrorMessages.email}
+            </p>
+          )}
           <Field name="password">
             {({ field, form, meta }) => (
               <input
@@ -53,6 +61,11 @@ const SignUpPage = ({ setStep }) => {
               />
             )}
           </Field>
+          {signUpErrorMessages.password && (
+            <p className="text-danger text-xs my-2">
+              {signUpErrorMessages.password}
+            </p>
+          )}
           <div className="w-full flex flex-col space-y-2">
             <Button
               textColor={"white"}
