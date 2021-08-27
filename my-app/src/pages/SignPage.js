ac64de25470289237b8d19670f54098993413e4b
Giancarlo Danves <gdanves@gmail.com>
2021-08-27 15:27:51 -0300

    rendering sign in or sign up pages with stepper

--- my-app/src/pages/SignPage.js
+++ my-app/src/pages/SignPage.js
@@ -1,16 +1,36 @@
-import { useState } from "react";
+import { useState, useContext, useEffect } from "react";
 import { SignInPage } from "./SignInPage";
 import SignUpPage from "./SignUpPage";
+import { AuthContext } from "../Authentication";
+import { useHistory } from "react-router-dom";
+import classNames from "classnames";
+
 const SignPage = () => {
   const [step, setStep] = useState(0);
+  const { logged } = useContext(AuthContext);
+  let history = useHistory();
 
+  useEffect(() => {
+    if (logged) {
+      history.push("/");
+    }
+  }, [logged, history]);
   return (
-    <div className="mx-auto items-center min-h-full">
-      {step === 0 ? (
-        <SignInPage setStep={setStep} />
-      ) : (
-        <SignUpPage setStep={setStep} />
-      )}
+    <div
+      className={classNames("mx-auto items-center min-h-full", {
+        hidden: logged,
+      })}
+    >
+      {(() => {
+        switch (step) {
+          case 0:
+            return <SignInPage setStep={setStep} />;
+          case 1:
+            return <SignUpPage setStep={setStep} />;
+          default:
+            return <SignInPage setStep={setStep} />;
+        }
+      })()}
     </div>
   );
 };
