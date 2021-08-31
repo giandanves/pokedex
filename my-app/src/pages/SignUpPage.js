222627e6fe9476439419b9d3f05ad0baed82e0e2
Giancarlo Danves <gdanves@gmail.com>
2021-08-31 14:01:55 -0300

    handling errors and submit with mutations and it's response

--- my-app/src/pages/SignUpPage.js
+++ my-app/src/pages/SignUpPage.js
@@ -4,7 +4,7 @@ import Button from "../components/Button";
 import { AuthContext } from "../Authentication";
 
 const SignUpPage = ({ setStep }) => {
-  const { createAccount, signUpErrorMessages } = useContext(AuthContext);
+  const { signUp } = useContext(AuthContext);
   return (
     <div className="py-6">
       <h2 className="text-base font-bold text-center">Sign Up</h2>
@@ -15,76 +15,75 @@ const SignUpPage = ({ setStep }) => {
           email: "",
           password: "",
         }}
-        onSubmit={(values) => {
-          console.dir(values);
-          createAccount(values.name, values.email, values.password);
+        onSubmit={async (values, actions) => {
+          const resp = await signUp(values);
+
+          if (resp !== "success") {
+            actions.setErrors(resp);
+          }
         }}
       >
-        <Form>
-          <Field name="name">
-            {({ field, form, meta }) => (
-              <input
-                type="text"
-                className="text-body-02 w-full h-12 px-2 border border-gray rounded-lg my-2"
-                placeholder={"Name"}
-                {...field}
-              />
+        {({ errors }) => (
+          <Form>
+            <Field name="name">
+              {({ field, form, meta }) => (
+                <input
+                  type="text"
+                  className="text-body-02 w-full h-12 px-2 border border-gray rounded-lg my-2"
+                  placeholder={"Name"}
+                  {...field}
+                />
+              )}
+            </Field>
+            {errors.name && (
+              <p className="text-danger text-xs my-2">{errors.name}</p>
             )}
-          </Field>
-          {signUpErrorMessages.name && (
-            <p className="text-danger text-xs my-2">
-              {signUpErrorMessages.name}
-            </p>
-          )}
-          <Field name="email">
-            {({ field, form, meta }) => (
-              <input
-                type="text"
-                className="text-body-02 w-full h-12 px-2 border border-gray rounded-lg my-2"
-                placeholder={"Email"}
-                {...field}
-              />
+            <Field name="email">
+              {({ field, form, meta }) => (
+                <input
+                  type="text"
+                  className="text-body-02 w-full h-12 px-2 border border-gray rounded-lg my-2"
+                  placeholder={"Email"}
+                  {...field}
+                />
+              )}
+            </Field>
+            {errors.email && (
+              <p className="text-danger text-xs my-2">{errors.email}</p>
             )}
-          </Field>
-          {signUpErrorMessages.email && (
-            <p className="text-danger text-xs my-2">
-              {signUpErrorMessages.email}
-            </p>
-          )}
-          <Field name="password">
-            {({ field, form, meta }) => (
-              <input
-                type="text"
-                className="text-body-02 w-full h-12 px-2 my-6 border border-gray rounded-lg"
-                placeholder={"Password"}
-                {...field}
-              />
+            <Field name="password">
+              {({ field, form, meta }) => (
+                <input
+                  type="text"
+                  className="text-body-02 w-full h-12 px-2 my-6 border border-gray rounded-lg"
+                  placeholder={"Password"}
+                  {...field}
+                />
+              )}
+            </Field>
+            {errors.password && (
+              <p className="text-danger text-xs my-2">{errors.password}</p>
             )}
-          </Field>
-          {signUpErrorMessages.password && (
-            <p className="text-danger text-xs my-2">
-              {signUpErrorMessages.password}
-            </p>
-          )}
-          <div className="w-full flex flex-col space-y-2">
-            <Button
-              textColor={"white"}
-              bg={"primary"}
-              type={"submit"}
-              addClass={"h-11"}
-            >
-              Sign up
-            </Button>
-            <Button
-              textColor={"primary"}
-              bg={"white"}
-              addClass={"h-11"}
-              onClick={() => setStep(0)}
-            >
-              Sign in
-            </Button>
-          </div>
-        </Form>
+            <div className="w-full flex flex-col space-y-2">
+              <Button
+                textColor={"white"}
+                bg={"primary"}
+                type={"submit"}
+                addClass={"h-11"}
+              >
+                Sign up
+              </Button>
+              <Button
+                textColor={"primary"}
+                bg={"white"}
+                addClass={"h-11"}
+                onClick={() => setStep(0)}
+              >
+                Sign in
+              </Button>
+            </div>
+          </Form>
+        )}
       </Formik>
     </div>
   );
